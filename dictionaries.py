ad9338b4d4e12df26583e1a4a1ef4bf98f0df05f
hb150676 <trinh.bhn2506@gmail.com>
2025-11-05 12:28:33 +0100

    update

--- dictionaries.py
+++ dictionaries.py
@@ -8,9 +8,18 @@ day_of_week={
 
 print(day_of_week['Monday'])
 
-exch_rates = {'PLN': 3.7. 'EUR': 0.8}
+exch_rates = {'PLN': 3.7, 'EUR': 0.8}
 print(exch_rates)
 exch_rates['PLN'] = 3.8
 exch_rates['USD'] = 1.0
 for k, v in exch_rates.items():
     print(f'{k}: {v}')
+print('----------')
+for k in exch_rates.keys():
+    print(f'{k}: {exch_rates[k]}')
+print('----------')
+print(exch_rates.keys()) # - dict_keys - special type
+print(list(exch_rates.keys())) # - regular list
+
+print(exch_rates.values())
+print(list(exch_rates.values()))
